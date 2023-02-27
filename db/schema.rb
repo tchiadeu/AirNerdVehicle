c0bc7a267e6d022c8db344138365c5891bf5b7fc
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-02-27 21:38:03 +0100

    add references user to vehicles as a foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_144342) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_203230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,8 +48,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_144342) do
     t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "vehicles"
+  add_foreign_key "vehicles", "users"
 end
